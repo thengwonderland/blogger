6c5de0a211832bd8b5df7ac5f3c53033c7f0e0b9
Sievtheng Dara <sievtheng@gmail.com>
2023-03-02 15:20:52 +0700

    Create user-model.js

---
+++ backend/models/user-model.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowercase: true
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      lowercase: true
+    },
+    profile: {
+      type: String,
+      required: true
+    },
+    hashed_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    about: {
+      type: String
+    },
+    role: {
+      type: Number,
+      default: 0
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    },
+    resetPasswordLink: {
+      data: String,
+      default: ''
+    }
+  },
+  { timestamps: true }
+);
+
+UserSchema.virtual('password')
+  .set(function(password) {
+    // create a temporary variable _password
+    this.hashed_password = password;
+
+    // generate salt
+    this.salt = this.makeSalt();
+
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this.hashed_password;
+  });
+
+UserSchema.methods = {
+  // when we get the plain password form the client,encrypt the plain password
+  // and compare it with the hashed version of it from database
+  // if passwords match, authenticate the user
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (error) {
+      console.error(error);
+      return '';
+    }
+  },
+
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  }
+};
+
+module.exports = mongoose.model('User', UserSchema);
