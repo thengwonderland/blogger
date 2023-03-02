33f3abfb0e574d0f2d339eebbd846aca77211910
Sievtheng Dara <sievtheng@gmail.com>
2023-03-02 15:21:52 +0700

    Delete formatDate.js

--- client/src/lib/formatDate.js
+++
@@ -1,9 +0,0 @@
-const formatDate = (date, locale = 'en-US') => {
-  if (!date) return null;
-
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const formattedDate = new Date(date);
-  return formattedDate.toLocaleDateString(locale, options);
-};
-
-export default formatDate;
