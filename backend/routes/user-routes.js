296202b779fc28dba191ce889443b5043dd4dabe
Sievtheng Dara <sievtheng@gmail.com>
2023-03-02 15:21:05 +0700

    Create user-routes.js

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const {
+  requireSignin,
+  adminMiddleWare,
+  authMiddleWare
+} = require('../controllers/auth-controller');
+const {
+  read,
+  publicProfile,
+  updateProfile,
+  uploadProfilePhoto
+} = require('../controllers/user-controller');
+
+router.get('/user/profile', requireSignin, authMiddleWare, read);
+router.get('/user/:username', publicProfile);
+router.put('/user/update', requireSignin, authMiddleWare, updateProfile);
+router.get('/user/photo/:username', uploadProfilePhoto);
+
+module.exports = router;
